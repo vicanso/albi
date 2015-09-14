6e4b020b164de0f3ae012150c42222eaa4ba8d82
vicanso <vicansocanbico@gmail.com>
2015-09-14 20:07:21 +0800

    commit

--- helpers/consul.js
+++ helpers/consul.js
@@ -5,6 +5,7 @@ const config = localRequire('config');
 const urlJoin = require('url-join');
 const util = require('util');
 const _ = require('lodash');
+const consul = require('consul-simple-client');
 
 exports.register = register;
 
@@ -12,7 +13,7 @@ exports.register = register;
  * [register 注册服务]
  * @return {[type]} [description]
  */
-function *register() {
+function* register() {
   let hostName = process.env.HOSTNAME;
   let hosts = fs.readFileSync('/etc/hosts', 'utf8');
   // etc hosts中的ip都是正常的，因此正则的匹配考虑的简单一些
@@ -26,36 +27,11 @@ function *register() {
     tags.push('prefix:' + config.appUrlPrefix);
   }
   tags.push('http-ping');
-  let registerData = {
-    Node : hostName,
-    Address : address,
-    Service : {
-      ID : hostName,
-      Service : config.app,
-      Port : config.port,
-      Address : address,
-      tags : _.uniq(tags)
-    }
-  };
-  let url = urlJoin(config.consul, '/v1/catalog/register');
-  yield put(url, registerData);
-}
-
-
-/**
- * [put description]
- * @param  {[type]} argument [description]
- * @return {[type]}          [description]
- */
-function *put(url, data) {
-
-  return yield new Promise(function(resolve, reject) {
-    request.put(url).send(data).end(function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
+  yield consul.register({
+    id: hostName,
+    service: config.app,
+    address: address,
+    port: config.port,
+    tags: _.uniq(tags)
   });
 }
