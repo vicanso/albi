7a93d030c5befe682df11fb288c32738272b3f1a
vicanso <vicansocanbico@gmail.com>
2015-10-14 23:00:03 +0800

    commit

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,15 @@
 'use strict';
-
 module.exports = home;
 
 /**
  * [home description]
  * @return {[type]} [description]
  */
-function *home(){
+function* home() {
   /*jshint validthis:true */
   let ctx = this;
   yield Promise.resolve();
   ctx.state.viewData = {
-    name : 'vicanso'
+    name: 'vicanso'
   };
 }
--- helpers/http-request.js
+++ helpers/http-request.js
@@ -1,13 +1,41 @@
 'use strict';
-const request = require('superagent');
+const request = require('superagent-extend');
 const _ = require('lodash');
 const util = require('util');
 const sdc = localRequire('helpers/sdc');
-
-exports.get = get;
-exports.timeout = 10 * 1000;
 var processing = 0;
 var processedTotal = 0;
+
+
+exports.get = get;
+
+request.timeout(10 * 1000);
+
+request.on('request', function (req) {
+  processing++;
+  processedTotal++;
+  sdc.increment('request.processing');
+  req._startAt = Date.now();
+});
+
+request.on('complete', function (req, res) {
+  processing--;
+  let use = Date.now() - req._startAt;
+  let statusCode = res.status;
+
+  let length = _.get(res, 'headers.content-length') || _.get(res,
+    'text.length') || 0;
+  let str = util.format('request "%s %s" %d %d %dms %d-%d', req.method, req
+    .url, statusCode, length, use, processing, processedTotal);
+
+  sdc.decrement('request.processing');
+  sdc.increment('request.status.' + statusCode);
+  sdc.timing('request.use', use);
+  console.info(str);
+
+});
+
+
 /**
  * [get 请求数据]
  * @param  {[type]} url     [description]
@@ -16,52 +44,16 @@ var processedTotal = 0;
  */
 function* get(url, headers) {
   let req = request.get(url);
-  _.forEach(headers, function(v, k) {
+  _.forEach(headers, function (v, k) {
     req.set(k, v);
   });
-  return yield handle(req);
-}
-
-
-/**
- * [handle description]
- * @param  {[type]} req [description]
- * @return {[type]}     [description]
- */
-function* handle(req) {
-  let res = {};
-  let start = Date.now();
-  processing++;
-  processedTotal++;
-  sdc.increment('request.processing');
+  let res;
   try {
-    res = yield new Promise(function(resolve, reject) {
-      req.timeout(exports.timeout).end(function(err, res) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(res);
-        }
-      });
-    });
-  } catch (err) {
+    res = yield req.done();
+  } catch (e) {
     err.type = 'http-request';
     err.extra = req.url;
     throw err;
-  } finally {
-    processing--;
-    let statusCode = res.statusCode || 0;
-    let length = _.get(res, 'headers.content-length') || _.get(res,
-      'text.length') || 0;
-    let use = Date.now() - start;
-    let str = util.format('request "%s %s" %d %d %dms %d-%d', req.method, req
-      .url, statusCode, length, use, processing, processedTotal);
-
-    sdc.decrement('request.processing');
-    sdc.increment('request.status.' + statusCode);
-    sdc.timing('request.use', use);
-    console.info(str);
   }
-
   return res;
 }
