41e8b28b14c6383378e8e79f487ed7f5741ec31d
vicanso <vicansocanbico@gmail.com>
2015-12-07 21:40:32 +0800

    reset all

--- helpers/debug.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const config = localRequire('config');
-const debug = require('debug')('jt.' + config.app);
-module.exports = debug;
--- routes/page.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = [{
-	route: '/',
-	template: 'home',
-	handler: 'home'
-}];
