1f0265d8bd9e6919cfe60c7d31769583f245f24e
Tree.Xie <vicansocanbico@gmail.com>
2016-12-23 23:33:45 +0800

    when an 'unhandledRejection` or `uncaughtException` trigger, the application will be safe restart

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 require('./helpers/local-require');
-
 localRequire('helpers/bluebird');
-localRequire('helpers/exception');
 localRequire('helpers/joi');
 localRequire('models');
 const config = localRequire('config');
 localRequire('helpers/server')(config.port);
 localRequire('tasks');
+const utils = localRequire('helpers/utils');
+
+process.on('unhandledRejection', (err) => {
+  console.error(`unhandledRejection:${err.message}, stack:${err.stack}`);
+  utils.checkToExit(3);
+});
+process.on('uncaughtException', (err) => {
+  console.error(`uncaughtException:${err.message}, stack:${err.stack}`);
+  utils.checkToExit(3);
+});
--- helpers/exception.js
+++
@@ -1,7 +0,0 @@
-process.on('unhandledRejection', (err) => {
-  console.error(`unhandledRejection:${err.message}, stack:${err.stack}`);
-});
-process.on('uncaughtException', (err) => {
-  // TODO should safe exit
-  console.error(`uncaughtException:${err.message}, stack:${err.stack}`);
-});
