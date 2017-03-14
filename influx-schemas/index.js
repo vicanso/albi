7c03cdef651fb1d4e0f8d222a61653f09e10e9ae
vicanso <vicansocanbico@gmail.com>
2017-03-14 22:30:02 +0800

    update docs for `influx-schemas` module

--- influx-schemas/index.js
+++ influx-schemas/index.js
@@ -1,9 +1,13 @@
 /**
- * 此模块定义influxdb的schme，对于写入influxdb的数据，
- * 虽然influxdb会根据第一次写入的字段决定数据类型，
+ * 此模块定义influxdb的schme，在`helpers/influx`中会使用定义的schema来初始化。
+ * 对于写入influxdb的数据，虽然influxdb会根据第一次写入的字段决定数据类型，
  * 不过使用的时候，最好还是把schme先定义好
+ * @module influx-schemas
  * @see {@link https://github.com/vicanso/influxdb-nodejs}
- * @module helpers/utils
+ * @example
+ * const schmeas = require('./influx-schemas');
+ * // {"mongoose": {"fields": ...} ...}
+ * console.info(schemas);
  */
 
 const schemas = {
