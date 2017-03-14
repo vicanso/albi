8f50109fec7528186167be64a62ee72f661032fb
vicanso <vicansocanbico@gmail.com>
2017-03-14 22:51:55 +0800

    update docs for `controllers` module

--- controllers/home.js
+++ controllers/home.js
@@ -1,1 +1,15 @@
+/**
+ * @module controllers/home
+ */
+
+/**
+ * 根据template，生成html，主要是网页是单页应用但是需要刷新时能加载到html
+ * @param {String} method `GET`
+ * @param session `no session`
+ * @param {String} routes `/`, `/login`, `/register`
+ * @param {String} template `home`
+ * @return {String} 返回render的html
+ * @example
+ * curl 'http://host/login'
+ */
 module.exports = ctx => ctx.set('Cache-Control', 'public, max-age=600');
